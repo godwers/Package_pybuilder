f43074aa3057f440e506dddd9e85a3d30f9c1686
godwers <gondes.historia@gmail.com>
2025-09-04 01:17:49 -0300

    éeeeeeeeeeeeeeeeeeeeeeee meu fi

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 
-cd $HOME || exit 1
+cd "$HOME" || exit 1
 git clone https://github.com/godwers/Package_pybuilder
+python Package_pybuilder/src/alias.py
--- src/main.sh
+++ src/main.sh
@@ -1,6 +1,39 @@
 #!/usr/bin/bash
 
-echo "What's the name of your project?"
-read -r PROJECT_NAME
-source build.sh "$PROJECT_NAME"
-echo "Completed! :)"
+PROJECT_NAME=$1
+function create_virtual_enviroment() {
+  mkdir "venv"
+  python -m venv venv/
+}
+
+function create_source_files() {
+  mkdir "$PROJECT_NAME"
+  cd "$PROJECT_NAME" || return 0
+  touch "__init__.py"
+  touch "main.py"
+  cd ..
+}
+
+function create_files() {
+  touch ".gitignore"
+  touch "setup.py"
+  touch "README.md"
+  touch "LICENSE"
+  touch "pyproject.toml"
+  mkdir "tests"
+  create_source_files "$PROJECT_NAME"
+}
+
+function package_builder() {
+  if [ ! -d "$PROJECT_NAME/" ]; then
+    mkdir "$PROJECT_NAME"
+  else
+    echo "There's already a project with this name."
+    exit 1
+  fi
+  cd "$PROJECT_NAME" || exit 1
+  create_files "$PROJECT_NAME"
+  create_virtual_enviroment
+}
+
+package_builder "$PROJECT_NAME"
